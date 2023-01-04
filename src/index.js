023e95764b6220a0d7f4d0de48fc39d0169fd8e7
koushil-mankali <koushilmankali@gmail.com>
2023-01-04 17:34:25 +0530

    added bootstarp css file in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // importing routes from routes/index.js file.
 import router from "./routes";
